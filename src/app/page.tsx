d72bcc65055a45aba809270ccc143183c2805120
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-03 13:32:03 -0400

    ui auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ const Page = () => {
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-2xl font-bold mb-4">Welcome to the Home Page</h1>
         <p className="mb-4">Please log in to access the content.</p>
-        <LoginLink className="bg-black rounded-[5px] text-white px-2">
+        <LoginLink className="bg-black rounded-[5px] text-white px-2 py-1 mb-5">
           Login
         </LoginLink>
-        <RegisterLink className="bg-black rounded-[5px] text-white px-2">
+        <RegisterLink className="bg-black rounded-[5px] text-white px-2 py-1 ">
           Register
         </RegisterLink>
       </div>
