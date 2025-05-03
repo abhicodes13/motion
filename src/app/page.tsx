16dcde0c73b68e4f263a5f850683fc132197b99a
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-03 10:59:17 -0400

    kinde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ const Page = () => {
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-2xl font-bold mb-4">Welcome to the Home Page</h1>
         <p className="mb-4">Please log in to access the content.</p>
-        <LoginLink className="bg-black p-2 text-white mb-5">Login</LoginLink>
+        <LoginLink className="bg-black p-3 text-white mb-5">Login</LoginLink>
         <RegisterLink className="bg-black p-2 text-white">
           Register
         </RegisterLink>
