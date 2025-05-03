a752d8627e72d8ec85022afc6a8949d1dabccb2c
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-03 10:56:29 -0400

    kinde client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "./AuthProvider";
+import { AuthProvider } from "./AuthProvider.jsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
