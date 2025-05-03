8d5b7b060d8a7be9ee9dac49a6b907b62ebd7a76
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-03 13:30:25 -0400

    kinde auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 import React from "react";
 import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
 
 import Read from "./components/Read";
-import { redirect } from "next/navigation";
 
 const Page = () => {
   const { isAuthenticated } = useKindeAuth();
   if (!isAuthenticated) {
-    redirect("https://scrollread.vercel.app/api/auth/login");
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold mb-4">Welcome to the Home Page</h1>
+        <p className="mb-4">Please log in to access the content.</p>
+        <LoginLink className="bg-black rounded-[5px] text-white px-2">
+          Login
+        </LoginLink>
+        <RegisterLink className="bg-black rounded-[5px] text-white px-2">
+          Register
+        </RegisterLink>
+      </div>
+    );
   }
   return (
     <>
