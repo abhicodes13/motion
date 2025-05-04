3af64b8c5f396982da9d66a07a9427a1d4795107
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-04 12:32:33 -0400

    frontpage update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,79 @@
 "use client";
-import React from "react";
+import { Stars } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, { useEffect } from "react";
+import { FiArrowRight } from "react-icons/fi";
+import {
+  useMotionTemplate,
+  useMotionValue,
+  motion,
+  animate,
+} from "framer-motion";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs";
 import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
 import { redirect } from "next/navigation";
 
-const Page = () => {
+const COLORS_TOP = ["#11FFAA", "#1E63C6", "#2E84CF", "#DD535C"];
+
+export default function AuroraHero() {
   const { isAuthenticated } = useKindeAuth();
+  const color = useMotionValue(COLORS_TOP[0]);
+
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: "easeInOut",
+      duration: 10,
+      repeat: Infinity,
+      repeatType: "mirror",
+    });
+  }, []);
+
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
+  const border = useMotionTemplate`1px solid ${color}`;
+  const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
   if (!isAuthenticated) {
     return (
-      <div className="flex flex-col items-center justify-center h-screen bg-slate-200">
-        <h1 className="text-2xl font-bold mb-1">Welcome to the ScrollRead</h1>
-        <p className="mb-4">Please log in to access the content.</p>
-        <div className="flex gap-2 mt-4">
-          <LoginLink className="bg-black rounded-[5px] text-white  p-3 hover:opacity-75">
-            Login
-          </LoginLink>
-          <RegisterLink className="bg-black rounded-[5px] text-white p-3 hover:opacity-75">
-            Register
-          </RegisterLink>
+      <motion.section
+        style={{
+          backgroundImage,
+        }}
+        className="relative grid min-h-screen place-content-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200"
+      >
+        <div className="relative z-10 flex flex-col items-center">
+          <span className="mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
+            Beta Now Live!
+          </span>
+          <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-medium leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
+            Welcome to ScrollRead
+          </h1>
+          <p className="my-6 max-w-xl text-center text-base leading-relaxed md:text-lg md:leading-relaxed">
+            Checkout what we're building here at ScrollRead
+          </p>
+          <motion.button
+            style={{
+              border,
+              boxShadow,
+            }}
+            whileHover={{
+              scale: 1.015,
+            }}
+            whileTap={{
+              scale: 0.985,
+            }}
+            className="group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
+          >
+            <LoginLink>Login </LoginLink>
+            <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
+          </motion.button>
         </div>
-      </div>
+
+        <div className="absolute inset-0 z-0">
+          <Canvas>
+            <Stars radius={50} count={2500} factor={4} fade speed={2} />
+          </Canvas>
+        </div>
+      </motion.section>
     );
   }
   redirect("/hello");
-};
-
-export default Page;
+}
