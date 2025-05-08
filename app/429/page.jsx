f1ebd027c029cc969bcf0312f466c38fa0f76c09
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-07 22:46:17 -0400

    ratelimit

--- app/429/page.jsx
+++ app/429/page.jsx
@@ -7,14 +7,15 @@ export default function RateLimitPage() {
       style={{ padding: "3rem", textAlign: "center" }}
       className="bg-black text-white h-screen flex flex-col gap-3"
     >
+      <h1 style={{ fontSize: "3rem", fontWeight: "bold" }}>ScrollRead</h1>
       <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>
         Too Many Requests
       </h1>
-      <p style={{ marginTop: "1rem" }}>
+      <p style={{ marginTop: "1rem", marginBottom: "2rem" }}>
         You’ve hit the rate limit. Please slow down and try again later.
       </p>
-      <a className="bg-black/70 mt-3 p-2 text-white" href="/">
-        Go Back
+      <a href="/">
+        <button className="bg-black/70 mt-3 p-2 text-white">Go back</button>
       </a>
     </div>
   );
