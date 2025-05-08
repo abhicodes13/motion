69e7e0ef6ea66934868e1afd4a82b6e33603c461
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-07 22:48:46 -0400

    ratelimit 429 page

--- app/429/page.jsx
+++ app/429/page.jsx
@@ -5,9 +5,9 @@ export default function RateLimitPage() {
     //Page for when too many requests hit from client side
     <div
       style={{ padding: "3rem", textAlign: "center" }}
-      className="bg-black text-white h-screen flex flex-col gap-3"
+      className="bg-black text-white h-screen flex flex-col gap-3 justify-center items-center"
     >
-      <h1 style={{ fontSize: "3rem", fontWeight: "bold" }}>ScrollRead</h1>
+      <h1 style={{ fontSize: "1rem", fontWeight: "bold" }}>ScrollRead</h1>
       <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>
         Too Many Requests
       </h1>
@@ -15,7 +15,7 @@ export default function RateLimitPage() {
         You’ve hit the rate limit. Please slow down and try again later.
       </p>
       <a href="/">
-        <button className="bg-black/70 mt-3 p-2 text-white">Go back</button>
+        <button className="bg-purple-600 mt-3 p-2 text-white">Go back</button>
       </a>
     </div>
   );
