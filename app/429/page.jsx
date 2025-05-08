dc381e6df28d8ee70f1b9922f867302232e04ef5
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-07 22:55:18 -0400

    ui fix ratelimit 429 page

--- app/429/page.jsx
+++ app/429/page.jsx
@@ -1,22 +1,35 @@
 // app/429/page.jsx
-
+import { Stars } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 export default function RateLimitPage() {
   return (
     //Page for when too many requests hit from client side
+
     <div
       style={{ padding: "3rem", textAlign: "center" }}
-      className="bg-black text-white h-screen flex flex-col gap-3 justify-center items-center"
+      className="bg-black text-white h-screen flex flex-col gap-3 justify-center items-center z-10"
     >
-      <h1 style={{ fontSize: "1rem", fontWeight: "bold" }}>ScrollRead</h1>
+      <h1
+        style={{ fontSize: "1rem", fontWeight: "bold", marginBottom: "2rem" }}
+      >
+        ScrollRead
+      </h1>
       <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>
         Too Many Requests
       </h1>
-      <p style={{ marginTop: "1rem", marginBottom: "2rem" }}>
+      <p style={{ marginTop: "1rem", marginBottom: "1rem" }}>
         You’ve hit the rate limit. Please slow down and try again later.
       </p>
       <a href="/">
-        <button className="bg-purple-600 mt-3 p-2 text-white">Go back</button>
+        <button className="bg-purple-600 mt-3 p-2 hover:opacity-70 rounded-md text-white">
+          Go back
+        </button>
       </a>
+      <div className="absolute inset-0 z-0">
+        <Canvas>
+          <Stars radius={50} count={200} factor={4} fade speed={3} />
+        </Canvas>
+      </div>
     </div>
   );
 }
