1166476e450a41615d25d10b413872b216b7fb35
AbhiCodes <chhettriabhishek9@gmail.com>
2025-05-07 22:42:41 -0400

    ratelimit ui fix

--- app/429/page.jsx
+++ app/429/page.jsx
@@ -5,7 +5,7 @@ export default function RateLimitPage() {
     //Page for when too many requests hit from client side
     <div
       style={{ padding: "3rem", textAlign: "center" }}
-      className="bg-black text-white"
+      className="bg-black text-white h-screen flex flex-col gap-3"
     >
       <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>
         Too Many Requests
@@ -13,7 +13,7 @@ export default function RateLimitPage() {
       <p style={{ marginTop: "1rem" }}>
         You’ve hit the rate limit. Please slow down and try again later.
       </p>
-      <a className="bg-black mt-3 p-2 text-white" href="/">
+      <a className="bg-black/70 mt-3 p-2 text-white" href="/">
         Go Back
       </a>
     </div>
